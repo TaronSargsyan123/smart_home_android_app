55bc98b2ac4b479eabb0260fc67fd061eaf45523
Taron Sargsyan <taronsargsyanunity@gmail.com>
2022-12-18 04:44:32 +0400

    add timer activity design

--- app/src/main/java/com/example/arduinoonoffremout/components/ROneVOne/ROneVOneTimer.java
+++ app/src/main/java/com/example/arduinoonoffremout/components/ROneVOne/ROneVOneTimer.java
@@ -5,33 +5,19 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import android.os.Bundle;
 import android.util.Log;
-
-import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.arduinoonoffremout.R;
 import com.google.android.material.slider.Slider;
 
 public class ROneVOneTimer extends AppCompatActivity {
 
-    Slider slider;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rone_vone_timer);
-        slider = findViewById(R.id.ROneVOneTimerSlider);
-        slider.addOnSliderTouchListener(new Slider.OnSliderTouchListener() {
-            @Override
-            public void onStartTrackingTouch(@NonNull Slider slider) {
+        setContentView(R.layout.activity_r_one_v_one_timer);
 
-            }
-
-            @Override
-            public void onStopTrackingTouch(@NonNull Slider slider) {
-                Log.i("Slider", String.valueOf(slider.getValue()));
-            }
-        });
     }
 }
