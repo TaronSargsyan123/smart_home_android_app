47bfc6a55fd1764a8129d8712ce60f90bcc992b6
Taron Sargsyan <taronsargsyanunity@gmail.com>
2022-12-30 22:01:18 +0400

    moved the bluetooth functionality to a separate file

--- app/src/main/java/com/example/arduinoonoffremout/CreateDeviceActivity.java
+++ app/src/main/java/com/example/arduinoonoffremout/CreateDeviceActivity.java
@@ -15,6 +15,7 @@ import android.app.Activity;
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
+import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.pm.PackageManager;
@@ -36,6 +37,8 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import com.example.arduinoonoffremout.bluetooth.BluetoothDefaultLogic;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -44,19 +47,14 @@ import java.util.Arrays;
 
 public class CreateDeviceActivity extends AppCompatActivity  {
     //static final String ACCESS_MESSAGE="ACCESS_MESSAGE";
-
-    BluetoothAdapter btAdapter;
     private ListView listView;
-
-    private OutputStream outputStream;
-    private InputStream inStream;
-    private ArrayList<BluetoothDevice> bluetoothDevices;
-
     private EditText nameEditText;
     private EditText ssidEditText;
     private EditText passwordEditText;
-
+    private BluetoothDefaultLogic bluetoothDefaultLogic;
     private BluetoothDevice device;
+    private ArrayList<BluetoothDevice> bluetoothDevices;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -65,27 +63,27 @@ public class CreateDeviceActivity extends AppCompatActivity  {
 
         Spinner dropdown = findViewById(R.id.spinner1);
 
-        btAdapter = BluetoothAdapter.getDefaultAdapter();
-        bluetoothDevices = new ArrayList<>();
+
         listView = (ListView) findViewById(R.id.bluetoothItemsList);
 
         ArrayAdapter<CharSequence>adapter=ArrayAdapter.createFromResource(this, R.array.devices, android.R.layout.simple_spinner_dropdown_item);
         dropdown.setAdapter(adapter);
 
+        bluetoothDefaultLogic = new BluetoothDefaultLogic(this, this);
 
+        bluetoothDefaultLogic.setActivityParent(this);
         nameEditText = findViewById(R.id.nameEditText);
         ssidEditText = findViewById(R.id.wifiSSidEditText);
         passwordEditText = findViewById(R.id.wifiPasswordEditText);
         TextView back = findViewById(R.id.backToDeviceList);
         String type;
 
-        if (btAdapter.isEnabled()){
+        if (bluetoothDefaultLogic.isBluetoothEnable()){
             drawList();
             listViewItemClick();
         }else {
-            enableBluetooth();
+            bluetoothDefaultLogic.enableBluetooth();
         }
-
 
         back.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -102,7 +100,16 @@ public class CreateDeviceActivity extends AppCompatActivity  {
                     try {
                         String message = dropdown.getSelectedItem().toString() + "/%" + "0.0.0.0" + "/%" + nameEditText.getText().toString();
                         sendMessage(message);
-                        send(device);
+                        SharedPreferences sharedPreferences = getSharedPreferences("Authorisation", Context.MODE_PRIVATE);
+                        String email = sharedPreferences.getString("email", null);
+                        String []temp = email.split("@");
+                        String deviceName = nameEditText.getText().toString();
+                        String userName = temp[0];
+                        String ssid = ssidEditText.getText().toString();
+                        String password =  passwordEditText.getText().toString();
+                        String s = userName + "$" + deviceName + "$" + ssid + "$" + password;
+
+                        bluetoothDefaultLogic.send(device, s);
                         //disableBluetooth();
                     } catch (Exception e) {
                         sendMessage("Fail");
@@ -113,92 +120,10 @@ public class CreateDeviceActivity extends AppCompatActivity  {
             }
         });
 
-
-
-
-
-
-
-
-
-    }
-
-    private void sendMessage(String message){
-
-        Intent data = new Intent();
-        data.putExtra(DevicesFragment.ACCESS_MESSAGE, message);
-        setResult(RESULT_OK, data);
-        finish();
-
-    }
-
-
-    private void disableBluetooth() {
-        if (btAdapter.isEnabled()) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                btAdapter.disable();
-            }
-
-        }
-    }
-
-    private void enableBluetooth() {
-        Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-        activityResultLauncher.launch(intent);
-    }
-
-    ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(
-            new ActivityResultContracts.StartActivityForResult(),
-            new ActivityResultCallback<ActivityResult>() {
-                @Override
-                public void onActivityResult(ActivityResult result) {
-                    if (result.getResultCode() == Activity.RESULT_OK && btAdapter.isEnabled()) {
-                        drawList();
-
-                    }
-                    else {
-                        enableBluetooth();
-                    }
-                }
-            });
-
-
-    private ArrayList<BluetoothDevice> refresh() {
-        bluetoothDevices.clear();
-        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-            for (BluetoothDevice device : btAdapter.getBondedDevices()) {
-                if (device.getType() != BluetoothDevice.DEVICE_TYPE_LE) {
-                    bluetoothDevices.add(device);
-                }
-            }
-        }
-        Log.i("SET", Arrays.toString(btAdapter.getBondedDevices().toArray()));
-
-
-        return bluetoothDevices;
-    }
-
-    private void drawList() {
-        refresh();
-
-
-
-        ArrayList<String> temp = new ArrayList<>();
-        for (BluetoothDevice device : bluetoothDevices) {
-            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                temp.add(device.getName() + " " + device.getAddress());
-            }
-
-        }
-
-        ArrayAdapter<String> adapter = new ArrayAdapter(this,
-                android.R.layout.simple_list_item_1, temp);
-
-
-        listView.setAdapter(adapter);
     }
 
     private void listViewItemClick(){
+        bluetoothDevices = bluetoothDefaultLogic.getBluetoothDevicesArray();
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
@@ -213,38 +138,40 @@ public class CreateDeviceActivity extends AppCompatActivity  {
 
     }
 
-    private void send(BluetoothDevice tempDevice){
-        if (ActivityCompat.checkSelfPermission(getApplicationContext(), Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+    private void sendMessage(String message){
 
-            ParcelUuid[] uuids = tempDevice.getUuids();
-
-            try (BluetoothSocket socket = tempDevice.createRfcommSocketToServiceRecord(uuids[0].getUuid())) {
-                socket.connect();
-                outputStream = socket.getOutputStream();
-                inStream = socket.getInputStream();
-                SharedPreferences sharedPreferences = getSharedPreferences("Authorisation",MODE_PRIVATE);
-                String email = sharedPreferences.getString("email", null);
-                String []temp = email.split("@");
-                String deviceName = nameEditText.getText().toString();
-                String userName = temp[0];
-                String ssid = ssidEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                String s = userName + "$" + deviceName + "$" + ssid + "$" + password;
-                outputStream.write(s.getBytes());
-                Toast.makeText(getApplicationContext(), tempDevice.getName(), LENGTH_LONG).show();
-            } catch (IOException e) {
-                e.printStackTrace();
-            }
-
-
-        }
-
+        Intent data = new Intent();
+        data.putExtra(DevicesFragment.ACCESS_MESSAGE, message);
+        setResult(RESULT_OK, data);
+        finish();
 
     }
 
 
 
 
+
+
+
+
+
+    private void drawList() {
+        bluetoothDevices = bluetoothDefaultLogic.getBluetoothDevicesArray();
+        bluetoothDefaultLogic.refresh();
+        ArrayList<String> temp = new ArrayList<>();
+        for (BluetoothDevice device : bluetoothDevices) {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                temp.add(device.getName() + " " + device.getAddress());
+            }
+
+        }
+
+        ArrayAdapter<String> adapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_1, temp);
+
+
+        listView.setAdapter(adapter);
+    }
 
 
 
